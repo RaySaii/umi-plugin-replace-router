2528726638e7f379f8f2e7afcb3e308779785ba7
王雷 <raysaii1991@gmail.com>
2019-07-06 16:54:19 +0800

    1.init

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 module.exports = function (api, options) {
   api.modifyEntryRender(_=>{
     return `window.g_isBrowser = true;
-  const rootContainer =  React.createElement(require('../router').default)
+  const rootContainer =  React.createElement(require('../../router').default)
   ReactDOM.render(
     rootContainer,
     document.getElementById('${api.config.mountElementId||'root'}'),
