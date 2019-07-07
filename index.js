8dc2860a48a60618318d0b04cc388f021323c411
王雷 <raysaii1991@gmail.com>
2019-07-07 11:50:57 +0800

    1.替换import

--- index.js
+++ index.js
@@ -8,16 +8,9 @@ module.exports = function (api, options) {
     const data = fs.readFileSync(routerPath).toString()
     fs.writeFileSync(routerPath,
         data.replace(/import\s*renderRoutes[\s\S]*?;/, '')
-            .replace(/const\s*Router[\s\S]*?;/,'')
-            .replace(/import[\s\S]*?react-router-dom['"];/,`import {renderRoutes,Router} from 'freya-router-dom';
-            `)
-            .replace(/return[\s\S]*/,`return (
-            <Router history={history}>
-                  {renderRoutes(routes,props)}
-             </Router>
-        )
-  }
-`),
+            .replace(/const\s*Router[\s\S]*?;/, '')
+            .replace(/import[\s\S]*?react-router-dom['"];/, `import {renderRoutes,Router} from 'freya-router-dom';
+            `),
         err => {
           api.log.error(err)
         })
