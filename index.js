52f3b6b8f7635a20fc84c311b969cfc153f87333
王雷 <raysaii1991@gmail.com>
2019-07-08 10:25:58 +0800

    1.增加构建前的文件改写

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ module.exports = function (api, options) {
           api.log.error(err)
         })
   })
+  api.beforeBuildCompileAsync( () => {
+    const routerPath = path.resolve(api.paths.tmpDirPath, 'router.js')
+    const data = fs.readFileSync(routerPath).toString()
+    fs.writeFileSync(routerPath,
+        data.replace(/import\s*renderRoutes[\s\S]*?;/, '')
+            .replace(/const\s*Router[\s\S]*?;/, '')
+            .replace(/import[\s\S]*?react-router-dom['"];/, `import {renderRoutes,Router} from 'freya-router-dom';
+            `),
+        err => {
+          api.log.error(err)
+        })
+  });
   api.modifyEntryHistory(_ => {
     return `
 require('freya-router-dom').createBrowserHistory({
