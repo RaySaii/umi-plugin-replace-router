596afc5c63e6342b3db03a3fb33d32d46e02146c
王雷 <raysaii1991@gmail.com>
2019-07-08 17:11:39 +0800

    1.添加重新生成router时

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const fs = require('fs')
 const path = require('path')
 module.exports = function (api, options) {
-  api.beforeDevServer(() => {
+  function modifyRouter() {
     const routerPath = path.resolve(api.paths.tmpDirPath, 'router.js')
     const data = fs.readFileSync(routerPath).toString()
     fs.writeFileSync(routerPath,
@@ -14,19 +14,12 @@ module.exports = function (api, options) {
         err => {
           api.log.error(err)
         })
-  })
-  api.beforeBuildCompileAsync( () => {
-    const routerPath = path.resolve(api.paths.tmpDirPath, 'router.js')
-    const data = fs.readFileSync(routerPath).toString()
-    fs.writeFileSync(routerPath,
-        data.replace(/import\s*renderRoutes[\s\S]*?;/, '')
-            .replace(/const\s*Router[\s\S]*?;/, '')
-            .replace(/import[\s\S]*?react-router-dom['"];/, `import {renderRoutes,Router} from 'freya-router-dom';
-            `),
-        err => {
-          api.log.error(err)
-        })
-  });
+  }
+
+  api.beforeDevServer(modifyRouter)
+  api.onGenerateFiles(modifyRouter)
+  api.beforeBuildCompileAsync(modifyRouter)
+
   api.modifyEntryHistory(_ => {
     return `
 require('freya-router-dom').createBrowserHistory({
