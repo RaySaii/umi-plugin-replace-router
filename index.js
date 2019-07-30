0c3f8d6fa7fb9cb09fdd00c25adc30c61165b4a5
王雷 <raysaii1991@gmail.com>
2019-07-30 21:46:45 +0800

    1.修改路由要做环境区分

--- index.js
+++ index.js
@@ -4,39 +4,45 @@ const fs = require('fs')
 const path = require('path')
 module.exports = function (api, options) {
 
-  function delay(dur) {
-    return new Promise(resolve => {
-      setTimeout(_ => {
-        resolve()
-      }, dur)
-    })
-  }
-
-  function modifyRouter() {
-    const routerPath = path.resolve(api.paths.tmpDirPath, 'router.js')
-    const data = fs.readFileSync(routerPath).toString()
-    fs.writeFileSync(routerPath,
-        data.replace(/import\s*renderRoutes[\s\S]*?;/, '')
-            .replace(/const\s*Router[\s\S]*?;/, '')
-            .replace(/import[\s\S]*?react-router-dom['"];/, `import {renderRoutes,Router} from 'freya-router-dom';
-            `),
-        err => {
-          api.log.error(err)
+    function delay(dur) {
+        return new Promise(resolve => {
+            setTimeout(_ => {
+                resolve()
+            }, dur)
         })
-  }
+    }
 
-  if(process.env.NODE_ENV=='production'){
-    api.beforeBuildCompileAsync(modifyRouter)
-  }else {
-    api.beforeDevServer(modifyRouter)
-    api.onGenerateFiles(modifyRouter)
-  }
+    function modifyRouter() {
+        const routerPath = path.resolve(api.paths.tmpDirPath, 'router.js')
+        try {
+            const data = fs.readFileSync(routerPath).toString()
+            fs.writeFileSync(routerPath,
+                data.replace(/import\s*renderRoutes[\s\S]*?;/, '')
+                    .replace(/const\s*Router[\s\S]*?;/, '')
+                    .replace(/import[\s\S]*?react-router-dom['"];/, `import {renderRoutes,Router} from 'freya-router-dom';
+            `),
+                err => {
+                    api.log.error(err)
+                })
+        } catch (e) {
+            console.log('======== error =========')
+            console.log(e)
+        }
 
-  api.modifyEntryHistory(_ => {
-    return `
+    }
+
+    if (process.env.NODE_ENV == 'production') {
+        api.beforeBuildCompileAsync(modifyRouter)
+    } else {
+        api.onGenerateFiles(modifyRouter)
+        api.beforeDevServer(modifyRouter)
+    }
+
+    api.modifyEntryHistory(_ => {
+        return `
 require('freya-router-dom').createBrowserHistory({
     basename: window.routerBase,
 })
     `.trim()
-  })
+    })
 }
